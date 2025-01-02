b526658c0ad7e114330447c578c1e02ad6c09427
user <user@DESKTOP-GAP52DF>
2025-01-02 18:55:46 +0100

    Ready to start implementing DB connection.

--- app/src/main/java/com/example/myapplication/model/GroceryItem.kt
+++ app/src/main/java/com/example/myapplication/model/GroceryItem.kt
@@ -1,7 +1,24 @@
 package com.example.myapplication.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/*
 data class GroceryItem(
     val id: Int = 0,
     val name: String,
     val quantity: Int
 )
+*/
+
+
+
+@Entity(tableName = "grocery_items")
+data class GroceryItem(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String,
+    val quantity: Int
+)
+
+
+
